54979e2bf3c847571cf0039809a9acbcf9d13080
Edery <tearbird99@naver.com>
2022-02-15 12:47:02 +0900

    플레이어 이동 1

--- Assets/Scripts/PlayerWalk.cs
+++ Assets/Scripts/PlayerWalk.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerWalk : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+
+    Rigidbody2D rigid;
+    float h;
+    float v;
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        speed = 100;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (h < 0) transform.localScale = new Vector3(-1, 1, 1);
+        else if (h > 0) transform.localScale = new Vector3(1, 1, 1);
+
+        rigid.velocity = new Vector2(h, v) * speed * Time.deltaTime;
     }
 }
